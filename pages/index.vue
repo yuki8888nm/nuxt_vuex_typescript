9b9a97bd42c8aadccbd859f9f698ef839c48181e
Oniki Yuki <>
2018-10-29 22:57:20 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="pa4">
+    <div class="bg-white-90 pa4">
+      <div class="f1">Nuxt TypeScript Starter</div>
+      <p>hoge: {{ $store.state.hoge }}</p>
+      <p>fuga: {{ $store.state.fuga }}</p>
+    </div>
+    <div class="flex flex-wrap ph2 justify-between bg-white-80">
+    </div>
+  </section>
+</template>
+<script>
+</script>
+
---
+++ store/index.ts
@@ -0,0 +1,26 @@
+export const state: () => any = (): any => ({
+  hoge: 'dddd',
+  fuga: 0,
+});
+
+export const mutations: any = {
+  rootChangeHoge(state, payload) {
+    state.hoge = payload.value;
+  },
+  rootChangeFuga(state, payload) {
+    state.fuga = payload.fuga;
+  },
+};
+
+export const actions: any = {
+  save({ commit }, payload) {
+    commit('rootChangeHoge', { value: payload.hoge });
+    commit('rootCchangeFuga', { value: payload.fuga });
+  },
+};
+
+export const getters: any = {
+  halfFuga(state) {
+    return state.fuga / 2;
+  },
+};
